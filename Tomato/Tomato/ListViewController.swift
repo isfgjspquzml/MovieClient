53670c24f9bf0042d078e46939afb73b3bb61392
Tianyu Shi <ts133@duke.edu>
2014-09-13 17:10:42 -0700

    Fix project errors

--- Tomato/Tomato/ListViewController.swift
+++ Tomato/Tomato/ListViewController.swift
@@ -18,25 +18,17 @@ class ListViewController: UIViewController, UITableViewDataSource {
         super.viewDidLoad()
         
         // Get Rotten Tomatoes DVDs
-//        let YourApiKey = "8upky4adwajswdr73bufgjwg"
-//        let RottenTomatoesURLString = "http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/top_rentals.json?apikey=" + YourApiKey
-//        let request = NSMutableURLRequest(URL: NSURL.URLWithString(RottenTomatoesURLString))
-//        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler:{ (response, data, error) in
-//            var errorValue: NSError? = nil
-//            let dictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &errorValue) //as? NSDictionary {
-////                self.moviesArray = dictionary["movies"] as? NSArray
-////                self.movieList.reloadData()
-////            } else {
-//                // TODO: Something went wrong
-////            }
-//        })
-        
         let YourApiKey = "8upky4adwajswdr73bufgjwg"
         let RottenTomatoesURLString = "http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/top_rentals.json?apikey=" + YourApiKey
         let request = NSMutableURLRequest(URL: NSURL.URLWithString(RottenTomatoesURLString))
         NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue.mainQueue(), completionHandler:{ (response, data, error) in
             var errorValue: NSError? = nil
-//            let dictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &errorValue) as NSDictionary
+            if let dictionary = NSJSONSerialization.JSONObjectWithData(data, options: nil, error: &errorValue) as? NSDictionary {
+                self.moviesArray = dictionary["movies"] as? NSArray
+                self.movieList.reloadData()
+            } else {
+                // TODO: Something went wrong
+            }
         })
         
     }
@@ -52,12 +44,12 @@ class ListViewController: UIViewController, UITableViewDataSource {
      func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = movieList.dequeueReusableCellWithIdentifier("com.tianyu.Rotten-Tomatoes.movieCell") as MovieTableViewCell
         let movieDictionary = self.moviesArray![indexPath.row] as NSDictionary
-//        cell.movieTitleLabel.text = movieDictionary["title"] as NSString
-//        cell.movieYearLabel.text = String(movieDictionary["year"] as Int)
-//        cell.movieRatingLabel.text = movieDictionary["mpaa_rating"] as NSString
-//        cell.movieScoreLabel.text = String((movieDictionary["ratings"] as NSDictionary)["audience_score"] as Int)
-//        cell.movieDescriptionLabel.text = movieDictionary["synopsis"] as NSString
-//        cell.movieDescriptionLabel.numberOfLines = 0;
+        cell.movieTitleLabel.text = movieDictionary["title"] as NSString
+        cell.movieYearLabel.text = String(movieDictionary["year"] as Int)
+        cell.movieRatingLabel.text = movieDictionary["mpaa_rating"] as NSString
+        cell.movieScoreLabel.text = String((movieDictionary["ratings"] as NSDictionary)["audience_score"] as Int)
+        cell.movieDescriptionLabel.text = movieDictionary["synopsis"] as NSString
+        cell.movieDescriptionLabel.numberOfLines = 0;
         
         return cell
     }
