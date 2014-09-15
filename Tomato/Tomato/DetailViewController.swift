76ee5b8adb37dd03f7477194bd01fa7009220c43
Tianyu Shi <ts133@duke.edu>
2014-09-14 17:49:35 -0700

    dynamically changing textview and other view

--- Tomato/Tomato/DetailViewController.swift
+++ Tomato/Tomato/DetailViewController.swift
@@ -12,9 +12,10 @@ class DetailViewController: UIViewController {
     
     @IBOutlet var detailView: UIView!
     @IBOutlet weak var posterImageView: UIImageView!
+    @IBOutlet weak var informationView: UIView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var ratingLabel: UILabel!
-    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var synopsisTextView: UITextView!
     @IBOutlet weak var runtimeLabel: UILabel!
     @IBOutlet weak var criticScoreLabel: UILabel!
     @IBOutlet weak var audienceScoreLabel: UILabel!
@@ -48,18 +49,47 @@ class DetailViewController: UIViewController {
             let audienceScore = (detailsDictionary!["ratings"] as NSDictionary)["audience_score"] as Int
             let criticScore = (detailsDictionary!["ratings"] as NSDictionary)["critics_score"] as Int
             var err: NSError?
-            var imageData :NSData = NSData.dataWithContentsOfURL(highResURL,options: NSDataReadingOptions.DataReadingMappedIfSafe, error: &err)
+            var imageData :NSData = NSData.dataWithContentsOfURL(imageURL,options: NSDataReadingOptions.DataReadingMappedIfSafe, error: &err)
             
             posterImageView.image = UIImage(data: imageData)
-            titleLabel.text = detailsDictionary!["title"] as NSString + "(" + String(detailsDictionary!["year"] as Int) + ")"
+            detailView.sendSubviewToBack(posterImageView)
+            titleLabel.text = detailsDictionary!["title"] as NSString + " (" + String(detailsDictionary!["year"] as Int) + ")"
             ratingLabel.text = detailsDictionary!["mpaa_rating"] as NSString
-            synopsisLabel.text = detailsDictionary!["synopsis"] as NSString
-            synopsisLabel.numberOfLines = 0;
-            runtimeLabel.text = String(detailsDictionary!["runtime"] as Int)
+            
+            synopsisTextView.text = detailsDictionary!["synopsis"] as NSString
+
+            // Resize the textview
+            var frameSize = CGSize(width: synopsisTextView.frame.size.width, height: CGFloat.max)
+            var frameHeight = synopsisTextView.sizeThatFits(frameSize)
+            println(frameHeight.height)
+            synopsisTextView.frame.size.height = frameHeight.height
+            
+            runtimeLabel.text = String(detailsDictionary!["runtime"] as Int) + " MIN"
             audienceScoreLabel.text = String(audienceScore)
             criticScoreLabel.text = String(criticScore)
             audienceScoreImageView.image = (audienceScore > 50) ? thumbsUp : thumbsDown
             criticScoreImageView.image = (criticScore > 50) ? thumbsUp : thumbsDown
+            
+            // Resize information view
+            var totalHeight: CGFloat = 0
+            for (index, view: UIView) in enumerate(informationView.subviews as [UIView]) {
+                if(totalHeight < view.frame.origin.y + view.frame.size.height) {
+                    totalHeight = view.frame.origin.y + view.frame.size.height;
+                }
+            }
+            informationView.frame.size.height = totalHeight
+            
+            
         }
     }
+    
+    
+//    @IBAction func informationViewDragged(sender: UIPanGestureRecognizer) {
+//        var translation: CGPoint = sender.translationInView(informationView)
+//        var top = (informationView.
+//        
+//        informationView.center = CGPointMake(informationView.center.x, informationView.center.y + translation.y/10)
+//        println(informationView.center.y)
+//    
+//    }
 }
